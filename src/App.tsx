512ecc8e2d458d0fbbc6de25c215783cdbfc286a
Ricardo Delfin <me@rdelfin.com>
2020-10-03 15:02:12 +0100

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import CardActions from '@material-ui/core/CardActions';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import CardActions from "@material-ui/core/CardActions";
 
 const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 function App() {
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
 
-    return (
-        <Container maxWidth="sm">
-            <Card className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                        Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        be{bull}nev{bull}o{bull}lent
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" onClick={() => alert("lolwat") }>
-                        Hello World
-                    </Button>
-                </CardActions>
-            </Card>
-        </Container>
-    );
+  return (
+    <Container maxWidth="sm">
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => alert("lolwat")}>
+            Hello World
+          </Button>
+        </CardActions>
+      </Card>
+    </Container>
+  );
 }
 
 export default App;
