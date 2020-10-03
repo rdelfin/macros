9dd4b387d9b9e41bbef8c4aa5c35a45fb7ffcb6e
Ricardo Delfin <me@rdelfin.com>
2020-10-03 12:25:15 +0100

    Added sample card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function App() {
-  return (
-    <Button variant="contained" color="primary" onClick={() => alert("lolwat") }>
-      Hello World
-    </Button>
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+      <Container maxWidth="sm">
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={() => alert("lolwat") }>
+              Hello World
+            </Button>
+          </CardActions>
+        </Card>
+    </Container>
   );
 }
 
