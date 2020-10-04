3c50603930727d512a39a5b47d33e515794ae211
Ricardo Delfin <me@rdelfin.com>
2020-10-04 12:25:56 +0100

    Added display of all macros off of ajax request

--- react/src/MacroCard.tsx
+++ react/src/MacroCard.tsx
@@ -22,21 +22,25 @@ const styles = createStyles({
     },
 });
 
-function MacroCard(props: WithStyles<typeof styles>) {
-    const { classes } = props;
+interface Props extends WithStyles<typeof styles> {
+    name: string;
+}
+
+function MacroCard(props: Props) {
+    const { classes, name } = props;
 
     return (
         <Card className={classes.root}>
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image="https://filmdaily.co/wp-content/uploads/2020/07/2020meme-01-2.jpg"
-                    title="Macro"
+                    image={"http://localhost:8000/macro/" + name}
+                    title={name}
                 />
             </CardActionArea>
             <CardActionArea className={classes.bottom_action}>
                 <Typography variant="h5" component="h5">
-                    Macro Name
+                    {name}
                 </Typography>
             </CardActionArea>
         </Card>
--- react/src/MacroList.tsx
+++ react/src/MacroList.tsx
@@ -3,41 +3,71 @@
  */
 
 import React from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { WithStyles, withStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import MacroCard from "./MacroCard";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        paper: {
-            height: 140,
-            width: 100,
-        },
-        control: {
-            padding: theme.spacing(2),
-        },
-    })
-);
+const styles = (theme: Theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+});
 
-function MacroList() {
-    const classes = useStyles();
-
-    return (
-        <Grid container className={classes.root} spacing={2}>
-            <Grid item xs={12}>
-                <Grid container justify="center" spacing={5}>
-                    {Array.from(Array(5).keys()).map((value) => (
-                        <Grid key={value} item>
-                            <MacroCard />
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-        </Grid>
-    );
+interface Response {
+    macros: Array<Macro>;
 }
 
-export default MacroList;
+interface Macro {
+    id: number;
+    name: string;
+}
+
+interface Props extends WithStyles<typeof styles> {}
+interface State {
+    macros: Array<Macro>;
+}
+
+class MacroList extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { macros: [] };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8000/macros")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Data: " + data);
+                this.setState({
+                    macros: data.macros,
+                });
+            });
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Grid container className={classes.root} spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={5}>
+                        {this.state.macros.map((value) => (
+                            <Grid key={value.name} item>
+                                <MacroCard name={value.name} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(MacroList);
