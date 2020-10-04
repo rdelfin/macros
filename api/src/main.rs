79389de4afd1647719714d09d61fc2378af38277
Ricardo Delfin <me@rdelfin.com>
2020-10-04 12:23:52 +0100

    Added cors allow all to API

--- api/src/main.rs
+++ api/src/main.rs
@@ -10,12 +10,11 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
 
-use rocket::{
-    http::{ContentType, Header},
-    response::Stream,
-};
+use anyhow::Result;
+use rocket::{http::Method, response::Stream};
 use rocket_contrib::json::JsonValue;
-use std::io::{self, prelude::*, Cursor};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use std::io::Cursor;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -37,8 +36,31 @@ fn get_macro(name: String) -> Stream<Cursor<Vec<u8>>> {
     Stream::chunked(Cursor::new(image_data), 100)
 }
 
-fn main() {
+fn main() -> Result<()> {
+    let allowed_origins = AllowedOrigins::all();
+    let cors = CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Delete]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        expose_headers: ["Content-Type", "X-Custom"]
+            .iter()
+            .map(ToString::to_string)
+            .collect(),
+        max_age: Some(42),
+        send_wildcard: false,
+        fairing_route_base: "/mycors".to_string(),
+        fairing_route_rank: 0,
+    }
+    .to_cors()?;
+
     rocket::ignite()
+        .attach(cors)
         .mount("/", routes![index, macros, get_macro])
         .launch();
+
+    Ok(())
 }
